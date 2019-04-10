c4e42540774f4f2c94484b9abe5c8a1955c86996
Ayushi Pandey <31280978+iAyushiP@users.noreply.github.com>
2019-04-10 22:46:21 +0530

    Added field validations and connected to API

--- AgroSearch/app/src/main/java/ap/efficient_farming/AddEquipment.java
+++ AgroSearch/app/src/main/java/ap/efficient_farming/AddEquipment.java
@@ -7,29 +7,47 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxCallback;
+import com.androidquery.callback.AjaxStatus;
+
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class AddEquipment extends AppCompatActivity //implements AdapterView.OnItemSelectedListener
         {
+            private String IMAGE_SELECTED="IMAGE SELECTED";
+            URLs urlObject = new URLs();
+            String BASE_URL = urlObject.URL_ADD_LISTING;
     private Spinner spinnerType, spinnerMode;
-    private String IMAGE_SELECTED="IMAGE SELECTED";
+    EditText ETitle, EDesc, ERate;
+    Button UploadImage, RegisterButton;
     public static final int GET_FROM_GALLERY = 3;
     private static final String[] type = {"Vehicles", "Irrigation Equipment", "Cultivation Tools", "Fertilization Tools","Consumables","Pest Control"};
     private static final String[] mode = {"per hour","per day","per week","per month"};
-    Button UploadImage, RegisterButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_equipment);
+        ETitle = findViewById(R.id.EquipmentTitle);
+        EDesc = findViewById(R.id.EquipmentDesc);
+        ERate = findViewById(R.id.EquipmentRate);
         spinnerType = findViewById(R.id.typeSpinner);
         spinnerMode = findViewById(R.id.SpinnerMode);
         UploadImage = findViewById(R.id.UploadImage);
@@ -37,7 +55,7 @@ public class AddEquipment extends AppCompatActivity //implements AdapterView.OnI
         RegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AddNewEquipment();
+                AttemptAddEquipment();
             }
         });
         UploadImage.setOnClickListener(new View.OnClickListener() {
@@ -108,7 +126,77 @@ public class AddEquipment extends AppCompatActivity //implements AdapterView.OnI
             }
         }
     }
-    protected void AddNewEquipment() {
+    protected void AttemptAddEquipment() {
+        // Reset errors
+        ETitle.setError(null);
+        EDesc.setError(null);
+        ERate.setError(null);
 
+        // Store values at the time of the login attempt.
+        String title = ETitle.getText().toString();
+        String desc = EDesc.getText().toString();
+        String rate = ERate.getText().toString();
+        Boolean cancel=false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(title)) {
+            ETitle.setError(getString(R.string.error_empty_field));
+            focusView = ETitle;
+            cancel = true;
+        }
+            else if(TextUtils.isEmpty(desc)) {
+            EDesc.setError(getString(R.string.error_empty_field));
+            focusView = EDesc;
+            cancel = true;
+            }
+            else if (TextUtils.isEmpty(rate)){
+            ERate.setError(getString(R.string.error_empty_field));
+            focusView = ERate;
+            cancel = true;
+        }
+        if (!cancel) AddNewEquipment();
     }
-}
+    private void AddNewEquipment() {
+        try {
+            HashMap<String, String> param = new HashMap<>();
+            param.put("listing_title", ETitle.getText().toString());
+            param.put("listing_desc", EDesc.getText().toString());
+            param.put("listing_rate", ERate.getText().toString());
+            //add remaining parameters
+
+            AQuery aq = new AQuery(this);
+            aq.ajax(BASE_URL, param,  JSONObject.class, new AjaxCallback<JSONObject>() {
+                @Override
+                public void callback(String url, JSONObject json, AjaxStatus status) {
+                    super.callback(url, json, status);
+
+                    Log.v("CALLBACK RECEIVED" , String.valueOf(json) + status);
+
+                    try {
+                        if (json != null) {
+                            JSONObject h = json.getJSONObject("success");
+
+                            Log.v("SUCCESS" ,"SUCCESS" );
+                            Toast.makeText(AddEquipment.this, "New Equipment Added", Toast.LENGTH_LONG).show();
+                            exitEquipmentForm();
+                        } else {
+                            Log.v("ERROR", "ERROR");
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }.method(AQuery.METHOD_POST));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+            public void exitEquipmentForm() {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            }
+    }
+
